1767f7f9d45b31b6f3f0a15d31dfc58cd0102d73
Ruslan.Odintsov <rodintso@tktk.ee>
2024-03-31 19:13:29 +0300

    week7

---
+++ NumberArrays/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] myNums = new int[10];
+Random rnd = new Random();
+//populating the array with a random numbers
+int sum = 0;
+for (int i = 0; i < myNums.Length; i++)
+{
+    myNums[i] = rnd.Next(1, myNums.Length + 1);
+    sum = sum + myNums[i];
+}
+foreach (int element in myNums)
+{
+    Console.WriteLine(element);
+}
+Console.WriteLine($"Total: {sum}");
---
+++ RandomElement/Program.cs
@@ -0,0 +1,6 @@
+﻿string[] snacks = { "sushi", "pizza", "poke bowl", "burger", "ice cream" };
+
+Random rnd = new Random();
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Today we gonna eat {snacks[randomIndex]}");
